9018824b7a7a0b8edbc81c2387ddf2305102b50e
TranDangKhoi <baits16@gmail.com>
2023-06-19 18:34:15 +0700

    :+1: Refactor code

--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,4 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 const uri = `mongodb+srv://${process.env.CLOUD_DB_USERNAME}:${process.env.CLOUD_DB_PASSWORD}@twittercluster0.wkhc8f0.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
