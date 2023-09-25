caf2140017267f8374f4d65e6a940575bd409051
Trần Đăng Khôi <baits16@gmail.com>
2023-09-25 14:57:27 +0700

    :+1: Remove sensitive information from get me services

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -122,7 +122,16 @@ class UsersServices {
   }
 
   async getMe(user_id: string) {
-    const user = await databaseService.users.findOne({ _id: new ObjectId(user_id) });
+    const user = await databaseService.users.findOne(
+      { _id: new ObjectId(user_id) },
+      {
+        projection: {
+          password: 0,
+          email_verify_token: 0,
+          forgot_password_token: 0,
+        },
+      },
+    );
     return user;
   }
 
