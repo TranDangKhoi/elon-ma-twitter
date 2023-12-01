e891f5038b5d9c2fabf83ad9ce9504e049d3b789
TranDangKhoi <baits16@gmail.com>
2023-12-01 18:56:14 +0700

    :heartbeat: Update env example and types for env

--- src/types.d.ts
+++ src/types.d.ts
@@ -13,6 +13,9 @@ declare global {
       JWT_SECRET_REFRESH_TOKEN: string;
       JWT_SECRET_EMAIL_VERIFY_TOKEN: string;
       JWT_SECRET_FORGOT_PASSWORD_TOKEN: string;
+      GOOGLE_OAUTH_CLIENT_ID: string;
+      GOOGLE_OAUTH_CLIENT_SECRET: string;
+      GOOGLE_OAUTH_REDIRECT_URI: string;
       ACCESS_TOKEN_EXPIRES_IN: string;
       REFRESH_TOKEN_EXPIRES_IN: string;
       EMAIL_VERIFY_TOKEN_EXPIRES_IN: string;
