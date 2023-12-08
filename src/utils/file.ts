7229713da2fbfef44d54dd437793af8b03d20c98
TranDangKhoi <baits16@gmail.com>
2023-12-08 16:57:06 +0700

    :+1: Improve image filtering

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -2,8 +2,7 @@ import { Request } from "express";
 import formidable from "formidable";
 import fs from "fs";
 import path from "path";
-const uploadDir = path.resolve("uploads");
-
+import { uploadDir } from "src/constants/constants";
 export const initFolder = () => {
   const uploadFolderPath = path.resolve("uploads");
   if (!fs.existsSync(uploadFolderPath)) {
@@ -24,15 +23,29 @@ export const formiddableSingleUploadHandler = (req: Request) => {
     // 10 * 1024 = 10KB => 10KB * 1024 = 10MB
     maxFileSize: 10 * 1024 * 1024,
     filter: function ({ mimetype, name, originalFilename }) {
-      console.log({ name, mimetype });
-      const isFileValid = Boolean(name === "image" && mimetype?.includes("images"));
+      console.log(mimetype);
+      const isFileValid = Boolean(mimetype?.includes("image"));
+      const isKeyValid = name === "image";
+      if (!isFileValid) {
+        form.emit("error" as "data", new Error("File type is not valid") as any);
+      }
+      if (!isKeyValid) {
+        form.emit(
+          "error" as "data",
+          new Error(`The key "${name}" in form-data is not valid, please replace it with "image" `) as any,
+        );
+      }
       return isFileValid;
     },
   });
   return new Promise((resolve, reject) => {
     form.parse(req, (err, fields, files) => {
+      console.log(files);
       if (err) {
         return reject(err);
+      }
+      if (!files.image) {
+        return reject(new Error("Can not upload empty stuffs"));
       }
       return resolve(files);
     });
