09603dd3a2b67609945f1ce2dbf848d685efc16c
Trần Đăng Khôi <baits16@gmail.com>
2024-07-13 17:47:03 +0700

    :apple: Update request handlers ignore type-checking

--- src/utils/requestHandlers.ts
+++ src/utils/requestHandlers.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Response, Request, RequestHandler } from "express";
 
-export const wrapRequestHandler = <P>(func: RequestHandler<P>) => {
+export const wrapRequestHandler = <P>(func: RequestHandler<P, any, any, any>) => {
   return async (req: Request<P>, res: Response, next: NextFunction) => {
     try {
       await func(req, res, next);
