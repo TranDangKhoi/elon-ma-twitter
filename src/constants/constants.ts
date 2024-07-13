2f190a2fe616b18140a4407ce3c086d2f19dfc6b
Trần Đăng Khôi <baits16@gmail.com>
2024-07-13 17:46:43 +0700

    :zap: Update constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -4,3 +4,6 @@ export const IMAGE_UPLOAD_DIR = path.resolve("uploads/images");
 export const IMAGE_UPLOAD_TEMP_DIR = path.resolve("uploads/images/temp");
 export const VIDEO_UPLOAD_DIR = path.resolve("uploads/videos");
 export const VIDEO_UPLOAD_TEMP_DIR = path.resolve("uploads/videos/temp");
+
+export const MAXIMUM_TWEET_CHILDREN_lIMIT = 50;
+export const MINIMUM_TWEET_CHILDREN_LIMIT = 1;
