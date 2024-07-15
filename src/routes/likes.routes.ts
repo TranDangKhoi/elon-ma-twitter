922bc7053d932c97e7de547b146a14fa1a580a2b
Trần Đăng Khôi <baits16@gmail.com>
2024-07-15 18:13:44 +0700

    :+1: Update savepoint

--- src/routes/likes.routes.ts
+++ src/routes/likes.routes.ts
@@ -4,6 +4,8 @@ import { accessTokenValidator, verifiedUserValidator } from "~/middlewares/users
 import { wrapRequestHandler } from "~/utils/requestHandlers";
 
 const likesRouter = Router();
+// TODO: Implement routes for unliking post, getting liked posts of a user, getting likes of a post (pagination)
+
 likesRouter.post("/", accessTokenValidator, verifiedUserValidator, wrapRequestHandler(likePostController));
 
 export default likesRouter;
